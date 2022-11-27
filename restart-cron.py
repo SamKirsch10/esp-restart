08ea9e57aae8ea1382ebbf5135d4de9f0e72de18
samkirsch10 <SamKirsch10@users.noreply.github.com>
2022-11-27 15:37:06 -0500

    fix spacing

--- restart-cron.py
+++ restart-cron.py
@@ -17,15 +17,15 @@ logging.basicConfig(format='[%(asctime)s] %(levelname)s:%(message)s', level=logg
 DEVICES = dict()
 
 client = mqtt.Client(
-	client_id="restart_cron",
-	transport="tcp",
-	protocol=mqtt.MQTTv5
-	)
+    client_id="restart_cron",
+    transport="tcp",
+    protocol=mqtt.MQTTv5
+    )
 
 
 def on_connect(client, userdata, flags, rc, v5config=None):
-	logging.debug(f"Connected to mqtt with status: [{ack(rc)}]")
-	client.subscribe("espresense/rooms/+/telemetry")    
+    logging.debug(f"Connected to mqtt with status: [{ack(rc)}]")
+    client.subscribe("espresense/rooms/+/telemetry")    
 
 def on_message(client, userdata, message):
     # print("message topic=",message.topic)
@@ -34,57 +34,57 @@ def on_message(client, userdata, message):
     room = message.topic.split("/")[2]
     logging.debug("Adding telemetry data")
     if room not in DEVICES:
-    	lastRestarted = datetime.now()
+        lastRestarted = datetime.now()
     else:
-    	# keep current time...
-    	lastRestarted = DEVICES[room]["lastRestarted"]
+        # keep current time...
+        lastRestarted = DEVICES[room]["lastRestarted"]
     DEVICES[room] = {
-    	"ip": data["ip"],
-    	"lastSeen": datetime.now(),
-    	"lastRestarted": lastRestarted
-    	}
+        "ip": data["ip"],
+        "lastSeen": datetime.now(),
+        "lastRestarted": lastRestarted
+        }
 
 def restart(room):
-	try:
-		res = requests.post(f"http://{DEVICES[room]['ip']}/restart")
-		DEVICES[room]["lastRestarted"] = datetime.now()
-	except ConnectionResetError:
-		pass
-	sleep(5)
+    try:
+        res = requests.post(f"http://{DEVICES[room]['ip']}/restart")
+        DEVICES[room]["lastRestarted"] = datetime.now()
+    except ConnectionResetError:
+        pass
+    sleep(5)
 
 def watcher():
-	logging.info("Starting watcher")
-	while True:
-		logging.debug("watcher loop")
-		for room in DEVICES:
-			timeDelta = (datetime.now() - DEVICES[room]["lastSeen"]).seconds
-			if timeDelta > 300:
-				logging.info(f"Room [{room}] hasn't posted a message in awhile...")
-				logging.info("Restarting room...")
-				restart(room)
-			elif (datetime.now() - DEVICES[room]["lastRestarted"]).seconds > 21600: # 6 hours
-				logging.debug("6 hour time window reached!")
-				logging.info(f"Restarting room. [{room}]..")
-				restart(room)
-			else:
-				logging.debug(f"Room [{room}] is still actively posting!")
+    logging.info("Starting watcher")
+    while True:
+        logging.debug("watcher loop")
+        for room in DEVICES:
+            timeDelta = (datetime.now() - DEVICES[room]["lastSeen"]).seconds
+            if timeDelta > 300:
+                logging.info(f"Room [{room}] hasn't posted a message in awhile...")
+                logging.info("Restarting room...")
+                restart(room)
+            elif (datetime.now() - DEVICES[room]["lastRestarted"]).seconds > 21600: # 6 hours
+                logging.debug("6 hour time window reached!")
+                logging.info(f"Restarting room. [{room}]..")
+                restart(room)
+            else:
+                logging.debug(f"Room [{room}] is still actively posting!")
 
-		sleep(60)
+        sleep(60)
 
 def run():
-	logging.info("Starting MQTT Listener")
-	client.on_connect=on_connect
-	client.on_message=on_message
-	client.username_pw_set(argv[2], argv[3])
-	properties=Properties(PacketTypes.CONNECT)
-	properties.SessionExpiryInterval=30*60 # in seconds
-	client.connect(argv[1], keepalive=300, properties=properties)
-	client.loop_start()
-	watcher()
+    logging.info("Starting MQTT Listener")
+    client.on_connect=on_connect
+    client.on_message=on_message
+    client.username_pw_set(argv[2], argv[3])
+    properties=Properties(PacketTypes.CONNECT)
+    properties.SessionExpiryInterval=30*60 # in seconds
+    client.connect(argv[1], keepalive=300, properties=properties)
+    client.loop_start()
+    watcher()
 
 
 if __name__ == '__main__':
-	if len(argv) < 3:
-		logging.error(f"This script takes three positional arguments: \"./{os.path.basename(__file__)} mqtt_host mqtt_user mqtt_pass\"")
-		exit(1)
-	run()
+    if len(argv) < 4:
+        logging.error(f"This script takes three positional arguments: \"./{os.path.basename(__file__)} mqtt_host mqtt_user mqtt_pass\"")
+        exit(1)
+    run()
